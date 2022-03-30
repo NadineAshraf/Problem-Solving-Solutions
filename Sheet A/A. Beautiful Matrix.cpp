5115be72800cb69f6456d3dc5ffc46867aa723a5
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-03-30 13:12:12 +0200

    Create A. Beautiful Matrix.cpp

---
+++ Sheet A/A. Beautiful Matrix.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	int i,j,arr[5][5];
+	for(int i=0 ; i<5 ; i++)
+	{
+		for(int j=0 ; j<5 ; j++)
+		{
+			cin>>arr[i][j];
+		}
+	}
+	for(int i=0 ; i<5 ; i++)
+	{
+		for(int j=0 ; j<5 ; j++)
+		{
+	      if(arr[i][j] == 1)
+	     {
+
+	    		  cout << abs(i - 2) + abs(j - 2) << endl;
+	    		  break;
+	     }
+
+	     }
+		}
+
+ return 0;
+}
