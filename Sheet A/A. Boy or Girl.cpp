238610336105c5c4d4f3d2f595eaf62f8f0cd7a2
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-03-30 18:28:27 +0200

    Update A. Boy or Girl.cpp

--- Sheet A/A. Boy or Girl.cpp
+++ Sheet A/A. Boy or Girl.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+
+int main()
+{
+	int count=0;
+	string name;
+	cin>>name;
+	for(int i=1 ; i<=name.size() ; i++)
+	{
+      count=i;
+	}
+	if(count%2 == 0)
+	{
+		cout<<"CHAT WITH HER! ";
+	}
+	else
+	{
+		cout<<"IGNORE HIM";
+	}
+	 return 0;
+}
