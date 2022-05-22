886e444d94cf4afc4f9efbf123e1f6eea85b1d1d
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-05-22 10:30:25 +0200

    Update A. Snacktower.cpp

--- Sheet A/A. Snacktower.cpp
+++ Sheet A/A. Snacktower.cpp
@@ -4,51 +4,27 @@ using namespace std;
 
 int main()
 {
-   int n,i,k;
-   cin>>n;
-   int arr[n];
-   for(i=0; i<n; i++)
-   cin>>arr[i];
-   for(i=0; i<n; i++)
-   {   
-       for(unsigned int k=n ; k==arr[i] || k!=arr[i]; k--)
-       {
-            if(k!=arr[i])
-           cout<<"\n";
-           if(k==arr[i])
-           { 
-               cout<<arr[i]<<" ";
-               for(unsigned int z=i-1 ; z<i  ; z--)
-               {
-                 cout<<arr[z]<<" ";
-               }
-           }
-           break;
-          
-       }
-       
-   
-if(arr[i+1] < n)
+    int i,n,m,x=0;
+    cin>>n;
+    int arr[n];
+    m=n;
+    for(int i=0; i<n; i++ )
+    cin>>arr[i];
+    for(int i=0; i<n; i++)
     {
-   for(int j=0; j<n; j++)
-   {
-       for(int k=i+1; k<j; k++)
-       {
-           
-           if(k!=arr[i+1])
-           cout<<"\n";
-             for(unsigned int z=i-1 ; z<i  ; z--)
-               {
-                 cout<<arr[z]<<" ";
-               }
-           
-       }
-      
-   }
-   
-    }    
-
-}
+        for(int j=0; j<n;j++)
+        {
+            if(arr[j]==m)
+            {
+                cout<<arr[j]<<" ";
+                m--;
+                break;
+            }
+            else if(j>=i)
+            cout<<endl;
+            
+        }
+    }
 
     return 0;
 }
