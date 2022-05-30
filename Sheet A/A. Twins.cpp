e6b4a4cec5b3e13ab1301d344e5ebab715260530
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-05-30 19:52:56 +0200

    Update A. Twins.cpp

--- Sheet A/A. Twins.cpp
+++ Sheet A/A. Twins.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-   int n,count=0;
-   cin>>n;
-   int arr[n];
-   for(int i=0; i<n; i++)
-   cin>>arr[i];
-   for(int j=0; j<n-1; j++)
-   {
-       for(int i=0; i<n; i++)
-       {
-           if(arr[j] <= arr[i])
-           count++;
-       }
-       break;
-   }
-   cout<<count;
+int main() {
+	 int n, sum1=0, count=0,sum2=0;
+	 cin>>n;
+	 
+	 
+	 int arr[n];
+	 
+	 for(int i=0;i<n;i++){
+		 cin>>arr[i];
+		 sum1+=arr[i];
+	 }
+	 
+	 sum1 =sum1/2;
+	 sort(arr,arr+n);
+	 for(int i=n-1;i>=0;i--){
+		 sum2+=arr[i];
+		 count++;
+		if(sum2>sum1){
+		 	break;
+		}
+
+
+	 }
+
+	 cout<<count;
 }
