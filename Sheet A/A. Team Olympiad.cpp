aa9be5a9277da2aa2227f4921ec1f2304dfcf0d0
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-07-10 18:57:58 +0200

    Update A. Team Olympiad.cpp

--- Sheet A/A. Team Olympiad.cpp
+++ Sheet A/A. Team Olympiad.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main()
 {
-    int n,gifted,arr[4][5001],count[4]= {0},nteam;
+    int n,members,arr[4][4],count[4]= {0},NoOfTeams;
     cin>>n;
     for(int i=1 ; i<=n ; i++)
-      {  cin>>gifted;
-      count[gifted]++;
-      arr[gifted][count[gifted]]=i;
+      {  cin>>members;
+      count[members]++;
+      arr[members][count[members]]=i;
       }
-nteam = min(min(count[1],count[2]),count[3]);
-cout<<nteam<<"\n";
-for(int i=1; i<=nteam; i++)
+NoOfTeams = min(min(count[1],count[2]),count[3]);
+cout<<NoOfTeams<<"\n";
+for(int i=1; i<=NoOfTeams; i++)
     cout<<arr[1][i]<<" "<< arr[2][i]<<" "<<arr[3][i]<<"\n";
     return 0;
 }
