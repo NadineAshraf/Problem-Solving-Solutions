16da71043c810b83239359d1bbf4981ada1cebbf
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-07-10 18:54:09 +0200

    Update A. Team Olympiad.cpp

--- Sheet A/A. Team Olympiad.cpp
+++ Sheet A/A. Team Olympiad.cpp
@@ -1,45 +1,19 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int n,k=0,l=0,m=0,count=1,timer=0;
+    int n,gifted,arr[4][5001],count[4]= {0},nteam;
     cin>>n;
-    int arr[n],matrix[3][n];
-    for(int i=0 ; i<n ; i++)
-        cin>>arr[i];
-    for(int i=0 ; i<n ; i++)
-    {
-        if(arr[i] == 1 )
-       {
-           matrix[0][k] = count;
-           count++;
-           k++;
-       }
-        if(arr[i] == 2)
-       {
-           matrix[0][l] = count;
-           count++;
-           l++;
-       }
-        if(arr[i] == 3 )
-       {
-           matrix[0][m] = count;
-           count++;
-           m++;
-       }
-        
-    }
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<i; j++)
-    {if(matrix[j][i]>0 )
-    }
-    timer++;
-    }
-    cout<<timer;
-    
-
+    for(int i=1 ; i<=n ; i++)
+      {  cin>>gifted;
+      count[gifted]++;
+      arr[gifted][count[gifted]]=i;
+      }
+nteam = min(min(count[1],count[2]),count[3]);
+cout<<nteam<<"\n";
+for(int i=1; i<=nteam; i++)
+    cout<<arr[1][i]<<" "<< arr[2][i]<<" "<<arr[3][i]<<"\n";
     return 0;
 }
