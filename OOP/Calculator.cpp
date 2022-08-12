cc61b693db8d05eb35e63c75066a2284d3828c48
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-08-12 13:32:44 +0200

    Create Calculator.cpp

---
+++ OOP/Calculator.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Calculator
+{
+    private:
+   
+    public:
+   int Add(int num1, int num2);   // Overloading all of them is called Add
+   int Add(int num1, int num2, int num3);
+   float Add(float num1, float num2);
+   string Add(string a, string b);
+   Calculator();
+   ~Calculator();
+    
+};
+Calculator::Calculator() //Initialization
+{
+    
+}
+Calculator::~Calculator()
+{
+    cout<<"Exit"<<"\n";
+}
+int Calculator::Add(int num1, int num2)
+{
+    return num1 + num2;
+}
+int Calculator::Add(int num1, int num2, int num3)
+{
+    return num1 + num2 + num3;
+}
+float Calculator::Add(float num1, float num2)
+{
+    return num1+num2;
+}
+string Calculator::Add(string a, string b)
+{
+    return a + " "+ b;
+}
+int main()
+{
+  Calculator c1;
+  c1.Add(3,7);
+    cout<<"This is version 1 "<<c1.Add(3,7)<<"\n";
+    cout<<"This is version 2 "<<c1.Add(3,7,10)<<"\n";
+    cout<<"This is version 3 "<<c1.Add("Hello","World")<<"\n";
+    return 0;
+}
