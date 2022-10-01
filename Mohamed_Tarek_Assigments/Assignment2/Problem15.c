459dfe13b190ac372f9e90e1f2d7b65dfb9a98ec
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-01 11:02:45 +0200

    Update Problem15.c

--- Mohamed_Tarek_Assigments/Assignment2/Problem15.c
+++ Mohamed_Tarek_Assigments/Assignment2/Problem15.c
@@ -3,19 +3,33 @@
 
 void CountOnes(unsigned int number)
 {
-    int count = 0,result=0,i;
+    int count = 0,result=0,i,flag=0;
     for(i=0; i<32;i++)
     {
        result= number%10;
        if(result ==1)
        {
-          
+          flag=1;
           count++;
          
        }
+       if(i>32)
+       {
+           
+           flag=0;
+           count=0;
+           break;
+       }
        number/=10;
     } 
+    if(flag == 0)
+    {
+    printf("There is an overflow");
+    }
+    else if(flag ==1)
+    {
       printf("No of Counts = %d",count);
+    }
 }
 
 
