5f34b9b81ccdabaac73e7ece1937fab36cda2ff7
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-01 12:31:27 +0200

    Create Problem19.c

---
+++ Mohamed_Tarek_Assigments/Assignment2/Problem19.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+
+int NoIsPowerOf3(int number)
+{
+    if(number == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        while(number!=0)
+        {
+          if(number%3 == 0)
+           {
+              if(number == 3)
+             {
+                return 0;
+             }
+           number/=3;
+           }
+          else 
+          {
+              return 1;
+          }
+        }
+    }
+}
+
+int main(void)
+{
+	int result,num;
+	printf("Please enter the number: ");
+	scanf("%d",&num);
+  result = NoIsPowerOf3(num);
+  if(result == 0)
+  {
+      printf("The number %d is power of 3",num);
+  }
+  else if(result == 1)
+  {
+      printf("The number %d is not power of 3",num);
+  }
+	return 0;
+}
