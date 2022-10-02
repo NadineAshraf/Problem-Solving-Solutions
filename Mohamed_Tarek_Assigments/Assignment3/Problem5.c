c5803d918a2cc3bc3a9f9d12c5e0e374cf6bab7b
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-02 10:48:49 +0200

    Create Problem5.c

---
+++ Mohamed_Tarek_Assigments/Assignment3/Problem5.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#define size 5
+
+int LinearSearch(int arr[],int number)
+{
+    int i,flag=0;
+  for(i=0;i<size;i++)
+  {
+      if(number == arr[i])
+      {
+          flag=1;
+          break;
+      }
+  }
+  if(flag ==1)
+  {
+      return i;
+  }
+}
+int main(void)
+{
+    int i,index, arr[size],num;
+     printf("Please enter the number: ");
+  scanf("%d",&num);
+	printf("Please enter the elements of array: ");
+	for(i=0;i<size; i++)
+	{
+	    scanf("%d",&arr[i]);
+	}
+	index = LinearSearch(arr,num);
+	printf("The index = %d",index);
+	return 0;
+}
