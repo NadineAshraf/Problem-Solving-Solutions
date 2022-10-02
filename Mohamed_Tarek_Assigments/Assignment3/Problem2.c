de2a91acb4460ff3458a3ba263149eb470e51db3
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-02 09:38:16 +0200

    Create Problem2.c

---
+++ Mohamed_Tarek_Assigments/Assignment3/Problem2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define TRUE  1
+#define FALSE 0
+
+int check_distinct(char s[])
+{
+	int i=0,j=0;
+	int results = TRUE;
+	/* the idea is compare each element with all the following array elements */
+	while(s[i] != '\0') /* loop until the end of the string */
+	{
+		j = i+1; /* start compare from the next character */
+		while(s[j] != '\0')
+		{
+			if(s[i] == s[j])
+			{
+				results = FALSE; /* this character is not distinct */
+			}
+			j++; /* to compare with the next character */
+		}
+		i++; /* to check the next character */
+	}
+	return results;
+}
+int main(void)
+{
+	char str[20];
+	int results;
+	printf("Please enter ther required string message : ");
+	gets(str);
+	printf("\n");
+	
+	results = check_distinct(str);
+	
+	if(results == TRUE)
+	{
+		printf("all characters are distinct in the string\n");
+	}
+	else
+	{
+		printf("characters are not distinct in the string\n");
+	}
+	return 0;
+}
