f2b09acaba25adaa4da25e6dfb3043cd8d6afa66
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-03 13:00:48 +0200

    Create Problem19.c

---
+++ Mohamed_Tarek_Assigments/Assignment3/Problem19.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+int Identical_Array(int array1_size, int *array1, int array2_size, int *array2)
+{
+  int i;
+  for(i=0; i<array1_size; i++)
+  {
+      if(array1[i] == array2[i])
+      {
+          return 0;
+      }
+      else
+      {
+          return 1;
+      }
+  }
+}
+int main() 
+{
+    int size, i,result;
+    printf("Enter the size of array: ");
+    scanf("%d",&size);
+    int arr1[size];
+    int arr2[size];
+    printf("Please enter the first array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d",&arr1[i]);
+    }
+     printf("Please enter the second array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d",&arr2[i]);
+    }
+  
+ result = Identical_Array(size, arr1, size, arr2);
+ if(result == 0)
+ {
+   printf("The two arrays are identical");
+  
+ }
+ else if (result == 1)
+{
+    printf("The two arrays are not identical");
+}  
+  
+  
+    return 0;
+}
