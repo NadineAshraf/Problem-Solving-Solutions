45b26e77108d7ea3ad90c8bb7477796a8aa77b6b
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-03 14:32:37 +0200

    Create Problem21.c

---
+++ Mohamed_Tarek_Assigments/Assignment3/Problem21.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+ static int size;
+int OutputArray[256];
+void Identical_Array(int LowerValue, int HigherValue , int *arr)
+{
+  int i,count=0,ReversedNo;
+  
+  if((LowerValue>= HigherValue) || ( HigherValue-1 == LowerValue))
+	{
+		arr[0] = 0xFF;
+		arr[1] = 0xFF;
+		printf("The values are= %d %d",arr[0],arr[1]);
+		count =2;
+		
+       printf("\nOutput array size = %d",count);
+	}
+	else
+	{
+	    
+	
+    for(i=HigherValue; i>= LowerValue; i-- )
+  {
+      
+      count++;
+  }
+  printf("The array will be: ");
+  for(i=size; i>0; i--)
+  {
+      
+      arr[i] = HigherValue;
+      printf("%d ",arr[i]);
+      HigherValue--;
+  }
+  
+  printf("\nOutput array size = %d",count);
+	}
+}
+
+int main() 
+{ 
+    int n1,n2,i;
+   
+    printf("Enter size of array: ");
+    scanf("%d",&size);
+    printf("LowerValue = ");
+    scanf("%d",&n1);
+     printf("HigherValue = ");
+    scanf("%d",&n2);
+    int arr[size];
+
+    Identical_Array(n1,n2,arr);
+   
+    return 0;
+}
