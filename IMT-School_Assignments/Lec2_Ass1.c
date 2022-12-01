3710d1f1da4164531bc2de37ab4a833f21c19134
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-12-01 17:27:25 +0200

    Update Lec2_Ass1.c

--- IMT-School_Assignments/Lec2_Ass1.c
+++ IMT-School_Assignments/Lec2_Ass1.c
@@ -1,48 +1,34 @@
 #include <stdio.h>
-void main(void)
-{ int x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,search;
-	printf("Enter Number 1: ");
-	scanf("%d",&x1);
-	printf("Enter Number 2: ");
-	scanf("%d",&x2);
-	printf("Enter Number 3: ");
-	scanf("%d",&x3);
-	printf("Enter Number 4: ");
-	scanf("%d",&x4);
-	printf("Enter Number 5: ");
-	scanf("%d",&x5);
-	printf("Enter Number 6: ");
-	scanf("%d",&x6);
-	printf("Enter Number 7: ");
-	scanf("%d",&x7);
-	printf("Enter Number 8: ");
-	scanf("%d",&x8);
-	printf("Enter Number 9: ");
-	scanf("%d",&x9);
-	printf("Enter Number 10: ");
-	scanf("%d",&x10);
-	printf("Enter the value to search: ");
-	scanf("%d",&search);
-	if(search==x1)
-	printf("value is exist at element number 1");
-	else if(search==x2)
-	printf("value is exist at element number 2");
-	else if(search==x3)
-	printf("value is exist at element number 3");
-	else if(search==x4)
-	printf("value is exist at element number 4");
-	else if(search==x5)
-	printf("value is exist at element number 5");
-	else if(search==x6)
-	printf("value is exist at element number 6");
-	else if(search==x7)
-	printf("value is exist at element number 7");
-	else if(search==x8)
-	printf("value is exist at element number 8");
-	else if(search==x9)
-	printf("value is exist at element number 9");
-	else if(search==x10)
-	printf("value is exist at element number 10");
-	else
-	printf("The number isn't found");
+
+int main()
+{
+   int i,arr[10],x,flag=0;
+   for(i=0; i<10; i++)
+   {
+       printf("Enter Number %d: ",i+1);
+       scanf("%d",&arr[i]);
+   }
+   printf("Enter the value to search: ");
+   scanf("%d",&x);
+   for(i=0; i<10; i++)
+   {
+       if(arr[i] == x)
+       {
+           printf("Value is exist at element number %d",i+1);
+           flag=0;
+           break;
+           
+           
+       }
+       else
+       {
+           flag =1;
+           
+       }
+   }
+   if(flag == 1)
+   {
+      printf("Number is not found"); 
+   }
+    return 0;
 }
