e93baa394ca0a66b541077a15da40e9cff231cee
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2023-01-15 08:36:53 +0200

    Update A. Anton and Danik.cpp

--- Sheet A/A. Anton and Danik.cpp
+++ Sheet A/A. Anton and Danik.cpp
@@ -9,27 +9,39 @@
 using namespace std;
 int main()
 {
-   int n;
-   cin>>n;
+   int n, A=0 , D=0;
    string s;
+   cin>>n;
+  
    cin>>s;
-   int A , D;
+   
      for(int i=0 ; i<n ; i++)
      {
        if(s[i] == 'A')
-	A++;
+       {
+        	A++;
+       }
        else if (s[i] == 'D')
-	D++;
+       {
+	      D++;
+       }
 
      }
 if(A > D)
+{
 	cout<<"Anton";
+}
 else if(D > A)
+{
 	cout<<"Danik";
+}
 else
+{
 	cout<<"Friendship";
+}
 
 return 0;
+
 }
 
 
