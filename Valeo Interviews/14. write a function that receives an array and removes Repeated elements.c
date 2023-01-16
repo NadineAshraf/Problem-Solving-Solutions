fa96782401e9914d00ac56a96f3b225782c53142
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2023-01-16 07:17:24 +0200

    Update 14. write a function that receives an array and removes Repeated elements.c

--- Valeo Interviews/14. write a function that receives an array and removes Repeated elements.c
+++ Valeo Interviews/14. write a function that receives an array and removes Repeated elements.c
@@ -31,8 +31,10 @@ int RemoveDuplication(int *arr, int n)
    // If current element is not equal to next element
     // then store that current element
     for (int i = 0; i < n - 1; i++)
+    {
         if (arr[i] != arr[i + 1])
             temp[j++] = arr[i];
+    }
   
     // Store the last element as whether it is unique or
     // repeated, it hasn't stored previously
@@ -40,7 +42,9 @@ int RemoveDuplication(int *arr, int n)
   
     // Modify original array
     for (int i = 0; i < j; i++)
+    {
         arr[i] = temp[i];
+    }
   
     return j;
 }
