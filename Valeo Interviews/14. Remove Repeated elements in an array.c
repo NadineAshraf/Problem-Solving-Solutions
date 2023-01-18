70467b6ded2267854f7121d5dc903ac7656ab110
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2023-01-18 12:25:29 +0200

    Update 14. Remove Repeated elements in an array.c

--- Valeo Interviews/14. Remove Repeated elements in an array.c
+++ Valeo Interviews/14. Remove Repeated elements in an array.c
@@ -7,13 +7,17 @@ int main()
     int i=0, j=0;
     for(j=0; j<n; j++)
     {
-        for(i=j+1; i<n-1; i++)
-        {
-            if(arr[j] == arr[i])
+          if(arr[j] != -1)
+          {
+            for(i=j+1; i<n-1; i++)
             {
-                arr[i] = -1;
+                if(arr[j] == arr[i])
+                {
+                    arr[i] = -1;
+                }
             }
-        }
+          }
+        
     }
     for(i=0; i<n; i++)
     {
