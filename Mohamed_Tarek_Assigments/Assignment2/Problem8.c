ec8619c71e4cd35eb8727ebf4be1be1d7d3e2c11
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2023-01-21 07:42:28 +0200

    Update Problem8.c

--- Mohamed_Tarek_Assigments/Assignment2/Problem8.c
+++ Mohamed_Tarek_Assigments/Assignment2/Problem8.c
@@ -1,30 +1,36 @@
-#include<stdio.h>
-
-void Prime(int num1, int num2)
+#include <stdio.h>
+void PRIME(int num1, int num2)
 {
-    int i;
-    printf("The prime numbers are:");
-   for(i=num1; i<num2;i++)
-   {
-       if(i ==2)
+    int i,j,flag=0;
+    printf("Prime numbers are: ");
+    for(i=num1; i<=num2; i++)
+    {
+       for(j=2; j<=(i/2); j++)
+       {
+           if(i%j == 0)
+           {
+              flag=1;
+              break;
+           }
+       }
+       if(flag == 0)
        {
            printf("%d ",i);
        }
-     else if(i%2 !=0 && i%3 !=0)
-     {
-      printf("%d ",i);
-     }
-   }
-
+       else
+       {
+           flag = 0;
+       }
+     
+    }
+    
 }
-int main(void)
+int main()
 {
-   int x,y;
-   printf("Please enter the first number: ");
-   scanf("%d",&x);
-    printf("Please enter the second number: ");
-   scanf("%d",&y);
-   Prime(x,y);
-   
+    int n1,n2;
+    printf("Please enter two number: ");
+    scanf("%d %d",&n1,&n2);
+    PRIME(n1,n2);
+
     return 0;
 }
