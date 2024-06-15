35c69aa7af2e27b6dcd1d7c349c2d8826a2be9ec
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2024-06-15 09:50:55 +0300

    Create 3. Write a Java program to divide two numbers and print them on the screen.java

---
+++ Java/3. Write a Java program to divide two numbers and print them on the screen.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Addition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter first number: ");
+        int num1= input.nextInt();
+        System.out.println("Please enter second number: ");
+        int num2= input.nextInt();
+        System.out.println(num1/num2);
+        
+        
+    }
+}
