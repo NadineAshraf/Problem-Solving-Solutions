07ed831c4750ade1b3cbc04642827108249855f2
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2024-06-15 10:23:47 +0300

    Create 12. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.java

---
+++ Java/12. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Average {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Please enter first number:");
+      float num1 = input.nextFloat();
+       System.out.print("Please enter second number:");
+      float num2 = input.nextFloat();
+       System.out.print("Please enter third number:");
+      float num3 = input.nextFloat();
+      System.out.println((num1+num2+num3)/3);
+        
+      
+        
+        
+    }
+}
