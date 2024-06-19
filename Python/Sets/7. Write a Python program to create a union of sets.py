2ba3a4c607f453bb272a3bbd6c05911e7734afb8
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2024-06-19 10:58:48 +0300

    Create 7. Write a Python program to create a union of sets.py

---
+++ Python/Sets/7. Write a Python program to create a union of sets.py
@@ -0,0 +1,9 @@
+setx = set(["green","blue"])
+sety = set(["blue","red"])
+setz = setx.union(sety)
+print(setz)
+
+set1 = set([1,2,3,4,5])
+set2 = set([2,3,8,9])
+sett = set1.union(set2)
+print(sett)
