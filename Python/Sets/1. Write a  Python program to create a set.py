cb4db93dbc63d60b8c3986006087aa2b26b28e3e
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2024-06-19 10:47:25 +0300

    Create 1. Write a  Python program to create a set.py

---
+++ Python/Sets/1. Write a  Python program to create a set.py
@@ -0,0 +1,11 @@
+print("Create a new set: ")
+x = set()
+print(x)
+print(type(x))
+print("\nCreate a non-empty set: ")
+n = set([0,1,2,3,4])
+print(n)
+print(type(n))
+a={1,2,3,'Nadine','Ashraf'}
+print(type(a))
+print(a)
