653a55955e4569501d17ef9e7a67f014a646a5b9
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2025-11-21 11:32:52 +0200

    Add sum.java to calculate the sum of two integers

---
+++ Java/Basic_MD/sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Enter first integer: ");
+        int number1 = input.nextInt();
+        System.out.print("Enter second integer: ");
+        int number2 = input.nextInt();
+        int sum = number1 + number2;
+        System.out.printf("Sum is %d%n", sum);
+    }
+}
