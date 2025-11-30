e5f109d061a6480551f8f3d3bdb2d3d51a3a5f20
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2025-11-30 11:11:05 +0200

    Create 5.Write a Python program to add an item to a tuple.py

---
+++ Python/Tuple/5.Write a Python program to add an item to a tuple.py
@@ -0,0 +1,10 @@
+tuplex = (4,6,2,8,3,1)
+print(tuplex)
+tuplex = tuplex +(9,)
+print(tuplex)
+tuplex = tuplex[:5] + (15,20,25) + tuplex[:5]
+print(tuplex)
+listx = list(tuplex)
+listx.append(30)
+tuplex = tuple(listx)
+print(tuplex)
