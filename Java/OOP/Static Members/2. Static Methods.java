893277598172160e115fcd8aa42e45c5fd54c133
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2025-12-02 12:31:56 +0200

    Create 2. Static Methods.java

---
+++ Java/OOP/Static Members/2. Static Methods.java
@@ -0,0 +1,16 @@
+class MathUtility{
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+
+}
+    class Main{
+    public static void main(String[] args)
+    {
+       int sum = MathUtility.add(10,9);
+       System.out.println("The sum: " + sum);
+        
+    }
+}
