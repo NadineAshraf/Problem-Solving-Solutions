48c91411350dd6dcb283cfd1558414931b2d94c7
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2026-06-26 13:28:14 +0300

    Create Write a Java program to create a class called "Counter" with a static variable count. Implement a constructor that increments count every time an object is created. Print the value of count after creating several objects.java

---
+++ Java/Static/Write a Java program to create a class called "Counter" with a static variable count. Implement a constructor that increments count every time an object is created. Print the value of count after creating several objects.java
@@ -0,0 +1,21 @@
+//Counter.java
+public class Counter
+{
+    private static int count =0;
+    public Counter()
+    {
+        count++;
+    }
+    public static int getCount()
+    {
+        return count;
+    }
+}
+class Main {
+    public static void main(String[] args) {
+ Counter c1 = new Counter();
+ Counter c2 = new Counter();
+ Counter c3 = new Counter();
+ System.out.println("Counter = " + Counter.getCount());
+    }
+}
