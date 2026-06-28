7fcd8f74cf9f18e5970120526aec2bd30e919551
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2026-06-28 11:39:04 +0300

    Add Java program to get character at specific index

---
+++ Java/String/1. Get Character at Index.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+  String str = "Java Exercises!";
+  System.out.println("The character at position 0: " + str.charAt(0));
+  System.out.println("The character at position 10: " + str.charAt(10));
+
+}
+}
