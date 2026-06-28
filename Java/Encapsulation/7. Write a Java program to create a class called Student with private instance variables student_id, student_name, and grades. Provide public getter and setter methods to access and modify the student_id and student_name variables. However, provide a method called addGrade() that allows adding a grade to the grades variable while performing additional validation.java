5822a44a5d02a7897f8c83769f1985a366ad5600
Nadine Ghaly <91349300+NadineAshraf@users.noreply.github.com>
2026-06-28 16:58:29 +0300

    Create 7. Write a Java program to create a class called Student with private instance variables student_id, student_name, and grades. Provide public getter and setter methods to access and modify the student_id and student_name variables. However, provide a method called addGrade() that allows adding a grade to the grades variable while performing additional validation.java

---
+++ Java/Encapsulation/7. Write a Java program to create a class called Student with private instance variables student_id, student_name, and grades. Provide public getter and setter methods to access and modify the student_id and student_name variables. However, provide a method called addGrade() that allows adding a grade to the grades variable while performing additional validation.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Student
+{
+    private int student_id;
+    private String student_name;
+    private List grades;
+    public int getStudent_id()
+    {
+        return student_id;
+    }
+    public void setStudent_id(int student_id)
+    {
+        this.student_id=student_id;
+    }
+    public String getStudent_name()
+    {
+        return student_name;
+    }
+    public void setStudent_name(String student_name)
+    {
+        this.student_name =student_name;
+    }
+    public List getGrades()
+    {
+        return grades;
+    }
+    public void addGrade(double grade)
+    {
+        if(grades = null)
+        {
+            grades = new ArrayList<>();
+        }
+        grades.add(grade);
+    }
+}
+//Main.java
+import java.util.ArrayList;
+import java.util.List;
+class Main {
+    public static void main(String[] args) {
+     Student student = new Student();
+     student.setStudent_id(1);
+     student.setStudent_name("Nadine Ashraf");
+     
+     student.addGrade(92.5);
+     student.addGrade(89.0);
+     student.addGrade(90.3);
+     
+     int student_id =student.getStudent_id();
+     String student_name = student.getStudent_name();
+     List<Double> grades = student.getGrades();
+     
+     System.out.println("Student ID: " + student_id);
+        System.out.println("Student Name: " + student_name);
+        System.out.println("Grades: " + grades);
+    }
+}
