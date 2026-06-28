c2f07314b61edd347e2294dc31c6abec7f2135d6
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2026-06-28 12:05:29 +0300

    Add Java program to convert string to uppercase

---
+++ Java/String/30. Convert to Uppercase.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(String[] args) {
+  String str = "run quickly";
+  String upper_str = str.toUpperCase();
+  System.out.println("UpperCase: "+ upper_str);
+  
+}
+}
