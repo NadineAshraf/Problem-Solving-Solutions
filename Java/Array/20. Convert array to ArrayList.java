b73346279d1f524dfb3393653c0dc6237e2f7ceb
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2026-06-28 10:34:09 +0300

    Create 20. Convert array to ArrayList.java

---
+++ Java/Array/20. Convert array to ArrayList.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+class Main {
+    public static void main(String[] args) {
+    String[] my_array = new String[]{"Python","Java","C","C++"};
+    ArrayList list = new ArrayList(Arrays.asList(my_array));
+    System.out.println(list);
+    }
+}
