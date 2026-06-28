56213a08eb614a602f7371fbc24266d549fe7a68
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2026-06-28 11:47:43 +0300

    Create 7. Concatenate Two Strings.java

---
+++ Java/String/7. Concatenate Two Strings.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(String[] args) {
+  String str1 = "PHP Exercises and ";
+  String str2 = "Python Exercises";
+  String result = str1.concat(str2);
+  System.out.print(result);
+}
+}
