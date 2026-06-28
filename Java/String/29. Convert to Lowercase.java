3f493bb5cc4b5dbf87d0acc04d47edc5f4873c75
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2026-06-28 12:04:01 +0300

    Add Java program to convert string to lowercase

---
+++ Java/String/29. Convert to Lowercase.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(String[] args) {
+  String str = "The Quick BroWn FoX!";
+  String lower_str = str.toLowerCase();
+  System.out.println("LowerCase: "+ lower_str);
+  
+}
+}
