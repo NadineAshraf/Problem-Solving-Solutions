ad06ffe9821bcee82fd4654b10ee27e53b90ade6
Nadine Ghaly <91349300+NadineAshraf@users.noreply.github.com>
2026-06-28 17:12:33 +0300

    Create 2.Write a Java program to create a class called "MathUtility" with a static method add that takes two integers and returns their sum. Demonstrate the usage of this static method in the main method without creating an instance of "MathUtility".java

---
+++ Java/Static/2.Write a Java program to create a class called "MathUtility" with a static method add that takes two integers and returns their sum. Demonstrate the usage of this static method in the main method without creating an instance of "MathUtility".java
@@ -0,0 +1,12 @@
+public class MathUtility
+{
+    public static int add(int a, int b)
+    {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+     int sum = MathUtility.add(10,9);
+     System.out.println("Sum = " + sum);
+    }
+}
