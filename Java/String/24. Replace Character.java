1d73a88140a6358923ab34fd8b1f4ec1e2803991
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2026-06-28 11:52:59 +0300

    Create 24. Replace Character.java

---
+++ Java/String/24. Replace Character.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(String[] args) {
+  String str = "Nadine Ashraf Adeeb Naguib";
+  String new_str = str.replace('a','s');
+  System.out.println("Original string: " + str);
+  System.out.println("New string: " + new_str);
+}
+}
